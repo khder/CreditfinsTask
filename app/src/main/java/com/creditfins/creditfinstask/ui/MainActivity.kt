eb37f4c3b2269eb4e230d6fb07e8ada55e9b17d8
Khedr <ahmedkhaderfcih@gmail.com>
2022-01-15 16:05:44 +0200

    submit projects files and organize projects into layers

---
+++ app/src/main/java/com/creditfins/creditfinstask/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.creditfins.creditfinstask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.creditfins.creditfinstask.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
